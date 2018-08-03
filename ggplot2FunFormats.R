b9e912e9aa605cc4667413f9dce7d553ba42b9df
Andre Violante <violante.andre@gmail.com>
2018-08-03 14:30:25 -0400

    Update ggplot2FunFormats.R

--- ggplot2FunFormats.R
+++ ggplot2FunFormats.R
@@ -1,33 +1,15 @@
-# Fields in the cereal dataset:
+library(ggplot2)  # All Plots
+library(scales)
+library(corrplot) # Plot 2 Correlations
+library(dplyr)
+library(maps)     # Plot 3
+library(ggthemes) # Plot 3
 
-# Name: Name of cereal
-# mfr: Manufacturer of cereal
-#  A = American Home Food Products;
-#  G = General Mills
-#  K = Kelloggs
-#  N = Nabisco
-#  P = Post
-#  Q = Quaker Oats
-#  R = Ralston Purina
-# type:
-#  cold
-#  hot
-# calories: calories per serving
-# protein: grams of protein
-# fat: grams of fat
-# sodium: milligrams of sodium
-# fiber: grams of dietary fiber
-# carbo: grams of complex carbohydrates
-# sugars: grams of sugars
-# potass: milligrams of potassium
-# vitamins: vitamins and minerals - 0, 25, or 100, indicating the typical percentage of FDA recommended
-# shelf: display shelf (1, 2, or 3, counting from the floor)
-# weight: weight in ounces of one serving
-# cups: number of cups in one serving
-# rating: a rating of the cereals (Possibly from Consumer Reports?)
-
-library(ggplot2)
-library(corrplot)
+#_____________________________________________________________________________#
+#__________________________                         __________________________#
+#__________________________   PLOT 1: Scatter Plot  __________________________#
+#__________________________                         __________________________#
+#_____________________________________________________________________________#
 
 cereal = read.csv('/Users/anviol/Desktop/Content/Datasets/cereal.csv')
 
@@ -63,5 +45,79 @@ ggplot(cereal, aes(x=rating, y=sugars)) +                                       
         axis.ticks.y = element_line(colour="black", size=1),                     # Y Axis Tick Marks
         axis.text.x = element_text(colour="darkorange", size=8, face="bold"),    # X Axis Labels
         axis.text.y = element_text(colour="darkorange", size=8, face="bold"))    # Y Axis Labels
+
+dev.off()
+
+#_____________________________________________________________________________#
+#__________________________                         __________________________#
+#__________________________ PLOT 2: Multi-Line Plot __________________________#
+#__________________________                         __________________________#
+#_____________________________________________________________________________#
+
+# Load New Data (https://archive.ics.uci.edu/ml/datasets/Dow+Jones+Index)
+dow_df = read.table('/Users/anviol/Desktop/Content/Datasets/dow_jones_index/dow_jones_index.data', sep = ",", header = TRUE)
+dow_df_sub = filter(dow_df, stock == c('DIS','WMT'))
+
+# Convert Character Date to Date and 'Better' Format
+dow_df_sub$date = format(as.Date(dow_df_sub$date, format = "%m/%d/%Y"),"%m-%d-%Y")
+
+# Remove $ Symbol and COnvert to Numeric
+dow_df_sub$open = as.numeric(gsub("\\$", "", dow_df_sub$open))
+dow_df_sub$high = as.numeric(gsub("\\$", "", dow_df_sub$high))
+dow_df_sub$low = as.numeric(gsub("\\$", "", dow_df_sub$low))
+
+# Higer Resolution Image
+tiff('ggplotFormatMagic2.tiff', units="in", width=7, height=4, res=300)
+
+ggplot(dow_df_sub, aes(x = date, y = open, group = stock, colour=stock)) +
+  geom_line() +
+  geom_line(mapping = aes(y = low), lty = "dashed", colour="gray") +                          # Adding Multiple Lines
+  geom_line(mapping = aes(y = high), lty = "dashed", colour = "gray") +
+  labs(color='', x="Date", y="Stock Price", title="Dow Jones: Disney and Walmart") +          # All-in-One Labels
+  scale_color_manual(labels = c("Disney", "Walmart"),                                         # Manually Change Label Names and Colors
+                     values = c("#520099", "#ffc220")) +
+  scale_y_continuous(labels = dollar) +                                                       # Y Label $ Format
+  theme_bw() +
+  theme(plot.title = element_text(hjust=0.5, vjust=-0.4, size=10, face="bold"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour="gray"),
+        axis.title.x = element_text(colour="firebrick", size=8, face="bold"),
+        axis.title.y = element_text(colour="firebrick", size=8, face="bold"),
+        axis.ticks.x = element_line(colour="gray", size=.5),
+        axis.ticks.y = element_line(colour="gray", size=.5),
+        axis.text.x = element_text(colour="gray48", size=8, face="bold", angle = 45, hjust=1), # Add x-axis Angle and Horizontal Adjustment
+        axis.text.y = element_text(colour="gray48", size=8, face="bold"),
+        legend.text = element_text(colour="black", size=6, face ="bold"),                      # Legend Design Options
+        #legend.title = element_text(colour=, size=, face = ),
+        legend.position = "bottom")
+
+dev.off()
+
+#_____________________________________________________________________________#
+#_____________________                                  ______________________#
+#_____________________  PLOT 3: Simple Map w/ Lat Long  ______________________#
+#_____________________                                  ______________________#
+#_____________________________________________________________________________#
+
+# Load Mobile Usage Data
+mobile_df = read.csv('/Users/anviol/Desktop/Content/Datasets/mobile_act_3mon.csv')
+
+# Set up Clean Map Background
+usa_map = ggplot() +
+  borders("state", colour = "gray85", fill = "gray80") +
+  theme_map() 
+
+# High Res Image
+tiff('ggplotFormatMagic3.tiff', units="in", width=7, height=4, res=300)
+
+# Plot
+usa_map +
+  geom_point(data=mobile_df,
+             aes(x = lon, y = lat),
+             colour = 'dodgerblue4',
+             alpha = .3) +
+  labs(title="Mobile Usage in United States: 3 Months") +
+  theme(plot.title = element_text(hjust=0.5, vjust=-0.4, size=8, face="bold"))
 
 dev.off()
